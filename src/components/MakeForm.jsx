98007823d45d86f700db1f27e14b26eedcad1fcc
y inoue <admin@wainoMacBook-Pro.local>
2021-10-10 13:06:12 +0900

    setUp react context provider

--- src/components/MakeForm.jsx
+++ src/components/MakeForm.jsx
@@ -1,17 +1,22 @@
 import React, { useReducer} from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers'
+import AppContext from '../contexts/AppContext'
 import EventForm from "./EventForm";
 import Events from "./Events";
+
+console.log({AppContext})
 
 const MakeForm = () => {
     const [state, dispatch] = useReducer(reducer, [])
 
     return (
-        <div className="container-fluid">
-            <EventForm state={state} dispatch={dispatch}/>
-            <Events state={state} dispatch={dispatch}/>
-         </div>
+        <AppContext.Provider value={'Hello, I am a Provider.'}>
+            <div className="container-fluid">
+                <EventForm state={state} dispatch={dispatch}/>
+                <Events state={state} dispatch={dispatch}/>
+            </div>
+        </AppContext.Provider>
     )
 }
 
